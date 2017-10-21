5a7c236f686ab0448bf5f509aed2229e44f315fc
yurlov43 <yurlov43@yandex.ru>
2017-10-21 16:23:37 +0300

    Поменены имена переменных и функций, добавлен argparse и используется форматирование для сложения строк

--- bars.py
+++ bars.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import argparse
 import os.path
 from math import sqrt
 
@@ -9,61 +10,60 @@ def load_data(filepath):
         return json.load(file_json)
 
 
-def get_biggest_bar(bars):
+def get_biggest_bars(bars_list):
     biggest_bar = max(
-        bars["features"],
+        bars_list,
         key=lambda x: x["properties"]["Attributes"]["SeatsCount"])
     biggest_bars = filter(
         lambda x: x["properties"]["Attributes"]["SeatsCount"] ==
         biggest_bar["properties"]["Attributes"]["SeatsCount"],
-        bars["features"])
+        bars_list)
     return biggest_bars
 
 
-def get_smallest_bar(bars):
+def get_smallest_bars(bars_list):
     smallest_bar = min(
-        bars["features"],
+        bars_list,
         key=lambda x: x["properties"]["Attributes"]["SeatsCount"])
     smallest_bars = filter(
         lambda x: x["properties"]["Attributes"]["SeatsCount"] ==
         smallest_bar["properties"]["Attributes"]["SeatsCount"],
-        bars["features"])
+        bars_list)
     return smallest_bars
 
 
-def print_list_bar(str, bars):
-    print(str)
-    for bar in bars:
-        print("\t" + bar["properties"]["Attributes"]["Name"])
+def print_bars_list(print_text, bars_list):
+    print(print_text)
+    for bar in bars_list:
+        print("{}{}".format("\t", bar["properties"]["Attributes"]["Name"]))
 
 
-def get_closest_bar(bars, longitude, latitude):
+def get_closest_bars(bars_list, user_longitude, user_latitude):
     closest_bar = min(
-        bars["features"],
+        bars_list,
         key=lambda x: sqrt(
-            (x["geometry"]["coordinates"][0] - longitude)**2 +
-            (x["geometry"]["coordinates"][1] - latitude)**2))
+            (x["geometry"]["coordinates"][0] - user_longitude)**2 +
+            (x["geometry"]["coordinates"][1] - user_latitude)**2))
     closest_bars = filter(
         lambda x: x["geometry"]["coordinates"] ==
         closest_bar["geometry"]["coordinates"],
-        bars["features"])
+        bars_list)
     return closest_bars
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        sys.exit("Ошибка: не указан файл.")
-    if len(sys.argv) > 2:
-        sys.exit("Ошибка: слишком много параметров.")
-    filepath = sys.argv[1]
-    if not os.path.exists(filepath):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath")
+    arg = parser.parse_args()
+    if not os.path.exists(arg.filepath):
         sys.exit("Ошибка: файл не найден.")
-    bars = load_data(filepath)
-    biggest_bars = get_biggest_bar(bars)
-    print_list_bar("Самые большие бары:", biggest_bars)
-    smallest_bars = get_smallest_bar(bars)
-    print_list_bar("Самые маленькие бары:", smallest_bars)
-    longitude = float(input('Введите долготу (начиная с 37.): '))
-    latitude = float(input('Введите широту (начиная с 55.): '))
-    closest_bars = get_closest_bar(bars, longitude, latitude)
-    print_list_bar("Самые близкие бары:", closest_bars)
+    bars = load_data(arg.filepath)
+    biggest_bars = get_biggest_bars(bars["features"])
+    print_bars_list("Самые большие бары:", biggest_bars)
+    smallest_bars = get_smallest_bars(bars["features"])
+    print_bars_list("Самые маленькие бары:", smallest_bars)
+    user_longitude = float(input('Введите долготу: '))
+    user_latitude = float(input('Введите широту: '))
+    closest_bars = get_closest_bars(
+        bars["features"], user_longitude, user_latitude)
+    print_bars_list("Самые близкие бары:", closest_bars)
